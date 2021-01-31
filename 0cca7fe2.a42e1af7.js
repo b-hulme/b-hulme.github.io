02f73f358ded47b4511abeeaefe56a5e62960804
gh-actions <actions@gihub.com>
2021-01-31 21:28:12 +0000

    Deploy website - based on 3058e5b80aa46710c682575a11545985ed3527c3

---
+++ 0cca7fe2.a42e1af7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{61:function(e){e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"enterprise-architecture-governance","name":"enterprise architecture governance","count":1,"permalink":"/blog/tags/enterprise-architecture-governance"}')}}]);
